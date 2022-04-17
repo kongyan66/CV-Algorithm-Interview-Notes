b2383350d05e7c308e72798ea38e6b0ee3f43830
kongyan66 <1042869685@qq.com>
2022-04-17 21:13:08 +0800

    updata

--- src/哈希表/202.快乐数.py
+++ src/哈希表/202.快乐数.py
@@ -1,1 +1,20 @@
-print(2//10)
+class Solution:
+    def isHappy(self, n: int) -> bool:
+      def getSum(num):
+        _sum = 0
+        while num:
+          _sum += (num % 10) ** 2  # 从个位开始求平方和
+          num = num // 10
+        return _sum
+
+      recorde = set()
+      while True:
+        n = getSum(n)
+        if n == 1:
+          return True
+
+        
+        if n in recorde:  # 如果出现重复数，说明陷入死循环了
+          return False
+        else:
+          recorde.add(n)  
