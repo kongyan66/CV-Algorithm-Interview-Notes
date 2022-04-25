813de3db87c6e1f375e7ba4fad75519ba0b07f72
kongyan66 <1042869685@qq.com>
2022-04-25 20:35:42 +0800

    [feat]:替换空格

--- src/code_test.py
+++ src/code_test.py
@@ -1,3 +1,4 @@
 a = [1, 2, 3]
-for i in range(0, len(a), 2):
-    print(a[i])
+print(a)
+a.extend([1,2])
+print(a)
--- src/字符串/jz-05.替换空格.py
+++ src/字符串/jz-05.替换空格.py
@@ -1,5 +1,6 @@
 # 题目：请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
 # 讲解：https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/solution/mian-shi-ti-05-ti-huan-kong-ge-ji-jian-qing-xi-tu-/
+
 # 暴力解法，重新开辟一段内存去保存结果，最简单，空间复杂度O(N) 时间复杂度O(N)
 class Solution1:
     def replaceSpace(self, s: str) -> str:
@@ -12,23 +13,23 @@ class Solution1:
       s = "".join(res)
       return s
 
-# 使用指针，在不新建字符串的情况下实现原地修改。
+# 使用指针，在不新建字符串的情况下实现原地修改。但需要知道修改后列表的长度，
 class Solution2:
     def replaceSpace(self, s: str) -> str:
         res = list(s)
         counter = s.count(" ")
         # 每碰到一个空格就多拓展两个格子，1 + 2 = 3个位置存’%20‘
-        res.extend([' ']*counter*2)
+        res.extend([' ']*counter*2)  # .extend(ListB)
         left = len(s) - 1
-        right = len(res) - 1
+        right = len(res) - 1 
 
         while left >= 0:
-          if res[left] != " ":
+          if res[left] != " ":      # 没有空格，就把当前值往后补   
             res[right] = res[left]
             right -= 1
           else:
-            res[right-2:right+1] = "%20"
-            right -= 3
+            res[right-2:right+1] = "%20" # 有空格， 腾出三个位置
+            right -= 3  
           left -= 1
-        s = "".join(res)
+        s = "".join(res)       # 列表转字符串
         return s
