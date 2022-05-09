d5747fb7fe4398e8856c5f60844ba9c464adbe46
kongyan66 <1042869685@qq.com>
2022-05-09 11:00:53 +0800

    [feat]:二叉树统一迭代方法

--- src/二叉树/144. 二叉树的前序遍历.py
+++ src/二叉树/144. 二叉树的前序遍历.py
@@ -44,3 +44,31 @@ class Solution:
         if node.left:
           stack.append(node.left)
       return result
+
+# 统一写法：标记法
+# 要处理的节点放入栈之后，紧接着放入一个空指针作为标记
+# 简单记： 前序遍历：中左右 -> 入栈顺序：右左中
+class Solution3:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        stack = []
+        if root is not None:
+            stack.append(root)
+      
+        while stack:
+            node = stack.pop()       # 将该节点弹出，避免重复操作，下面再将右中左节点添加到栈中
+            # 节点按顺序入栈：右中左 这样出栈顺序刚好对应过来
+            if node is not None:
+                # 关键之处：中节点需要做标记，其他遍历顺序也是这做
+                stack.append(node)   # 添加中节点
+                stack.append(None)   # 空节点入栈，作为标记，表明该节点还未处理
+                if node.left:
+                    stack.append(node.left)   # 添加左节点（空节点不入栈）
+                if node.right:       # 添加右节点（空节点不入栈）
+                    stack.append(node.right)
+
+           # 把栈中节点值拿出来
+            else:                    #  只有遇到空节点的时候，才将下一个节点放进结果集
+                node = stack.pop()   #  重新取出栈中元素
+                result.append(node.val)
+        return result
