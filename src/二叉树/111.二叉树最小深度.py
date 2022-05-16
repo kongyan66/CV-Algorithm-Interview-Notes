b2075168d65a9f6d62364d9b4e9c1ac75dceb9ca
kongyan66 <1042869685@qq.com>
2022-05-16 10:29:11 +0800

    111.二叉树最小深度-递归版

--- src/二叉树/111.二叉树最小深度.py
+++ src/二叉树/111.二叉树最小深度.py
@@ -1,8 +1,8 @@
 # 题目：给定一个二叉树，找出其最小深度。
-# 说明：最小深度是从根节点到最近叶子节点的最短路径上的节点数量，叶子节点是指没有子节点的节点。
+# 说明：最小深度是从根节点到最近叶子节点的最短路径上的节点数量（叶子节点是指没有子节点的节点）
 
 # 思路：套用层序遍历模板，遇到子叶节点就返回计数器结果
-
+# 解法一：迭代法
 class Solution:
     def minDepth(self, root: TreeNode) -> int:
         if not root:
@@ -19,3 +19,30 @@ class Solution:
                     que.append(cur.right)
                 if cur.left == None and cur.right == None:
                     return count
+
+# 解法二：递归法
+# 可见递归法关注的是局部事件，比如此题，我们算好一个节点的深度，递归后就能算出一个树的深度
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        # 1.确定入参和出参
+        return self.getdepth(root)
+
+    def getdepth(self, node):
+        # 2.确定停止条件
+        if node is None:
+            return 0
+        # 3.确定单层递归逻辑
+        left_depth = self.getdepth(node.left)
+        right_depth = self.getdepth(node.right)
+        # z这里容易出错：最小深度是从根节点到最近叶子节点的最短路径上的节点数量，如果左子叶无节点，深度并不一定是1
+        # 如果左子叶为空，就看右子叶最小深度
+        if node.right == None and node.left != None:
+            return left_depth + 1
+        # 如果右子叶为空，就看左子叶为空
+        elif node.right != None and node.left == None:
+            return right_depth + 1
+        # 看左右子叶的最小深度
+        else:
+            return min(left_depth, right_depth) + 1
