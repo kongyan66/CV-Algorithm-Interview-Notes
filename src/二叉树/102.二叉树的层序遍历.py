994fe65d940f46d6bc2afe953e5e3de64f005604
kongyan66 <1042869685@qq.com>
2022-06-06 11:14:38 +0800

    [re]:102.二叉树层序遍历

--- src/二叉树/102.二叉树的层序遍历.py
+++ src/二叉树/102.二叉树的层序遍历.py
@@ -1,25 +1,48 @@
 # 题目：给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
 # 考察：队列  队列先进先出，符合一层一层遍历的逻辑，而是用栈先进后出适合模拟深度优先遍历也就是递归的逻辑。
 
-# 迭代法
+# 解法一： 迭代法
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
-        from collections import deque
+        if not root:
+            return []
+        que = [root]
         results = []
-        if not root:         # 空节点就返回
-            return results
-        que = deque([root])  # 构建一个双端序列装每一层的节点
-        while que:           # 每一轮循环就弹出该层节点的值
-            size = len(que)  
-            result = []
-            for _ in range(size):
-                node = que.popleft()
-                result.append(node.val)
-                if node.left:
-                    que.append(node.left)
-                if node.right:
-                    que.append(node.right)
-            results.append(result)
+        
+        while que:
+            res =[]
+            # 遍历当前层的宽度
+            for _ in range(len(que)):
+                # 每次遍历，当前层所有节点均处理完（弹完了）
+                cur = que.pop(0)
+                res.append(cur.val)
+                if cur.left:
+                    que.append(cur.left)
+                if cur.right:
+                    que.append(cur.right)
+            results.append(res)
         return results
 
-# 递归法
+# 解法二：递归法  这个递归着实不太好理解
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+      res = []
+      self.recursion(root, 0, res)        # 头结点的深度为0
+      return res 
+    # 1.确定入参&返回值
+    def recursion(self, node, depth, res):
+        # 2.确定终止条件
+        if not node:
+            return 
+        # 3.确定单层递归逻辑
+        # 为当前层分配一个空列表
+        if len(res) == depth:   # 这个很巧妙啊，res的长度刚好是depth, 省去一个局部变量List存每一层的值
+            res.append([])
+        # 将当前节点值放入列表
+        res[depth].append(node.val)
+        # 处理左右子节点
+        if node.left:
+            self.recursion(node.left, depth+1, res)
+        if node.right:
+            self.recursion(node.right, depth+1, res)
+        
