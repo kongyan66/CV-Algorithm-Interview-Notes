21f5c41be9bd7ed9beb4d4a0010b0b4353f6ff72
kongyan66 <1042869685@qq.com>
2022-06-07 15:02:25 +0800

    [re]:104. 二叉树的最大深度

--- src/二叉树/104.二叉树最大深度.py
+++ src/二叉树/104.二叉树最大深度.py
@@ -3,10 +3,10 @@
 # 解法一：迭代法
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        que = [root]
         count = 0
+        if not root:
+            return count
+        que = [root]
         while que:
             for _ in range(len(que)):
                 cur = que.pop(0)
@@ -14,23 +14,23 @@ class Solution:
                     que.append(cur.left)
                 if cur.right:
                     que.append(cur.right)
-            count += 1
+            count +=1 
         return count
 
 # 解法二：递归法
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
+        return self.recursion(root)
+    # 1.确定入参与返回值
+    # 返回值就是当前节点的深度depth
+    def recursion(self, node):
+        # 2.确定终止条件
+        # 此题有返回值，故终止的时候也要有返回值（空节点深度为0）
+        if not node:
             return 0
-        # 1.确定入参和出参
-        return self.getdepth(root)
-
-    def getdepth(self, node):
-        # 2.确定终止条件：如果为空节点的话，就返回0，表示高度为0
-        if node is None:
-            return 0
-        # 3.单层递归逻辑：先求它的左子树的深度，再求的右子树的深度，  
-        # 最后取左右深度最大的数值 再+1 （加1是因为算上当前中间节点）就是目前节点为根节点的树的深度。
-        left_depth = self.getdepth(node.left)
-        right_depth = self.getdepth(node.right)
-        return max(left_depth, right_depth) + 1
+        # 3.确定单层递归逻辑
+        # 分别求左右子树的深度，至于为啥不需要判断节点是否存在了，不存在深度就为0呗
+        left_depth = self.recursion(node.left)
+        right_depth = self.recursion(node.right)
+        # 这里需要注意下，是从当前节点往下看，所以最终深度需要+1
+        return max(left_depth, right_depth)+1
