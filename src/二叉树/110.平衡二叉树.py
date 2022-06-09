57ee61d690f0b3694dd5be260bce86b91f467449
kongyan66 <1042869685@qq.com>
2022-06-09 11:16:47 +0800

    [re]:110.二叉平衡树

--- src/二叉树/110.平衡二叉树.py
+++ src/二叉树/110.平衡二叉树.py
@@ -2,29 +2,33 @@
 # 二叉树节点的深度：指从根节点到该节点的最长简单路径边的条数。
 # 二叉树节点的高度：指从该节点到叶子节点的最长简单路径边的条数。本质上就是参考系不一样
 
-# 思路：遍历所有节点的左右子节点高度，比较高度差
-
-# 解法一：层序遍历
-class Solution:
+# 思路：遍历所有节点,比较左右子节点高度差
+# 解法一：迭代法 前序遍历+层序遍历
+class Solution1:
+    # 用前序遍历去遍历所有节点，然后比较每个节点左右子树的高度差
     def isBalanced(self, root: TreeNode) -> bool:
-        if root is None:
+        if not root:
             return True
-        que = [root]
-        while que:
-            cur = que.pop(0)
-            if abs(self.getdepth(cur.right) - self.getdepth(cur.left)) > 1:
+        stack = [root]
+        while stack:
+            cur = stack.pop()
+            left_height = self.get_height(cur.left)
+            right_height = self.get_height(cur.right)
+            if abs(left_height - right_height) > 1:
                 return False
 
-            if cur.left:
-                que.append(cur.left)
             if cur.right:
-                que.append(cur.right)        
+                stack.append(cur.right)
+            if cur.left:
+                stack.append(cur.left)
+
         return True
-    def getdepth(self, node):
-        if node is None:
-            return 0
+    # 用层序遍历获得节点的高度
+    def get_height(self, node):
+        height = 0
+        if not node:
+            return height
         que = [node]
-        count = 0
         while que:
             for _ in range(len(que)):
                 cur = que.pop(0)
@@ -32,8 +36,8 @@ class Solution:
                     que.append(cur.left)
                 if cur.right:
                     que.append(cur.right)
-            count += 1
-        return count
+            height += 1
+        return height
 
 # 递归法
 class Solution:
@@ -51,9 +55,11 @@ class Solution:
         # 3.确定单层递归逻辑
         left_height = self.getheight(root.left)
         right_height = self.getheight(root.right)
-        if left_height == -1 or right_height == -1:
+        if left_height == -1 or right_height == -1: 
             return -1
+        # 如果高度差大于一，则记当前节点高度为-1
         if abs(left_height - right_height) > 1:
             return -1
+        # 符合要求，就返回当前节点的高度
         else:
             return 1 + max(left_height, right_height)
