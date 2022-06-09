e775aca62d5194def347e78535831837d99a1788
kongyan66 <1042869685@qq.com>
2022-06-09 16:57:01 +0800

    [feat]:77.组合

--- src/code_test.py
+++ src/code_test.py
@@ -1,35 +1,7 @@
-def strStr(haystack, needle):
-    a = len(needle)
-    b = len(haystack)
-    if a == 0:
-        return 0
-    next = getnext(a, needle)
-    p = -1
-    for j in range(b):
-        while p >= 0 and needle[p + 1] != haystack[j]:
-            p = next[p]
-        if needle[p + 1] == haystack[j]:
-            p += 1
-        if p == a - 1:
-            return j - a + 1
-    return -1
-
-
-def getnext(a, needle):
-    next = ['' for i in range(a)]
-    k = -1
-    next[0] = k
-    for i in range(1, len(needle)):
-        while k > -1 and needle[k + 1] != needle[i]:
-            k = next[k]
-        if needle[k + 1] == needle[i]:
-            k += 1
-        next[i] = k
-    return next
-
-
-haystack ="hello"
-needle ="ll"
-l = strStr(haystack, needle)
-print(l)
-
+a = [1,2]
+b = []
+b.append(a)
+print(b)
+a.pop()
+a.pop()
+print(b)
