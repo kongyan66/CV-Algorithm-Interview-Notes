4ac14cb0d1285047c7671952ba1e4a5c795de382
kongyan66 <1042869685@qq.com>
2022-06-11 22:25:15 +0800

    [feat]:17.电话号码的字母组合

--- src/code_test.py
+++ src/code_test.py
@@ -1,12 +1,1 @@
-def outer_fun():
-    a = 1
-    def fun():
-        global  a # a为全局变量，与上面等于1的 a 没有关系
-        a = 3 # 定义全局变量
-        print(a) # 输出3
-        a = 2
-    fun()
-    print(a) #输出1，局部变量
-outer_fun()
-print(a) # 输出2，全局变量
 
