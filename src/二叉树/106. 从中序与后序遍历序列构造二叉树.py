92f63f45ce589173395eb9710af8dae1b4ebecfe
kongyan66 <1042869685@qq.com>
2022-06-12 10:41:28 +0800

    [re]:103.从中序与后序遍历序列构造二叉树

--- src/二叉树/106. 从中序与后序遍历序列构造二叉树.py
+++ src/二叉树/106. 从中序与后序遍历序列构造二叉树.py
@@ -5,25 +5,29 @@
 
 # 解法一：递归
 class Solution:
+    # 1.确定入参与返回值
+    # 返回值：二叉树的根节点
     def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        # 2.确定递归停止条件
         # 第一步：排除特殊情况 如果为空，说明树为空
-        if len(postorder) == 0:
+        if not postorder:
             return None
-        # # 第二步: 后序遍历的最后一个就是当前的中间节点. 
-        root_val = postorder[-1]
-        root = TreeNode(root_val)
-        # 第三步: 找切割点. 
-        separator_index = inorder.index(root_val)
-        # 第四步：切割inorder数组. 得到inorder数组的左,右半边.
-        inorder_left = inorder[:separator_index]
-        inorder_right = inorder[separator_index+1:]
-       # 第五步：同一棵树，所以可以由inposder的左右长度，得到postorder数组的左右半边
-        postorder_left = postorder[:len(inorder_left)]
-        postorder_right = postorder[len(inorder_left):len(postorder)-1]
-        # 第六步：递归
-        root.left = self.buildTree(inorder_left, postorder_left)
-        root.right = self.buildTree(inorder_right, postorder_right)
-
+        # 3.确定单层递归逻辑
+        # 找出左子叶和右子叶数组
+        # 第二步：从postorder末尾获取根节点值
+        rootValue = postorder[-1]
+        root = TreeNode(rootValue)
+        # 第三步： 找切割点 把数组分为左右两块 最重要一步
+        sparator_point = inorder.index(rootValue)
+        # 第四步:通过rootValue分割inorder
+        in_leftnode = inorder[:sparator_point]
+        in_rightnode = inorder[sparator_point+1:]
+        # 第五步：通过inorder的左右子树的长度来分割postorder
+        post_leftnode = postorder[:len(in_leftnode)]
+        post_rightnode = postorder[len(in_leftnode):len(postorder)-1]
+        # 第六步：使用递归
+        root.left = self.buildTree(in_leftnode, post_leftnode)
+        root.right = self.buildTree(in_rightnode, post_rightnode)
         return root
 
 # 解法二：迭代法
