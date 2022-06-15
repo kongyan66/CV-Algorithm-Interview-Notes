a2038aeb4c72f3e12602e6ef2b44c48c7ac37e76
kongyan66 <1042869685@qq.com>
2022-06-15 11:17:08 +0800

    [re]:530.二叉树搜索树最小绝对差

--- src/code_test.py
+++ src/code_test.py
@@ -1,1 +1,11 @@
-
+def t():
+    a = 2
+    def tt():
+        nonlocal a
+        print(a)
+        a += 2
+        print(a)
+    tt()
+    print(a)
+    print(a+2)
+t()
--- src/二叉树/530. 二叉搜索树的最小绝对差.py
+++ src/二叉树/530. 二叉搜索树的最小绝对差.py
@@ -1,5 +1,6 @@
 # 题目：给你一个二叉搜索树的根节点 root ，返回 树中任意两不同节点值之间的最小差值 
-# 思路：和700.二叉树中搜索类似， 用中序遍历构建一个有序序列就很容易了
+# 思路：和700.二叉树中搜索类似， 用中序遍历构建一个有序序列（单调递增），那么任意两节点的最小差值就转换为
+# 相邻节点的最小差值
 
 # 解法一： 迭代法 中序遍历
 
@@ -20,4 +21,22 @@ class Solution:
         return res
 
 
-# 解法二：递归法
+# 解法二：递归法 思路和迭代法法一致
+class Solution:
+    def getMinimumDifference(self, root: TreeNode) -> int:
+        pre = None
+        minvalue = float('INF')
+        def recursion(root):
+            nonlocal pre
+            nonlocal minvalue
+            if not root:
+                return
+            recursion(root.left)
+            cur = root
+            if pre:
+                minvalue = min(minvalue, cur.val - pre.val)
+            pre = cur
+            recursion(root.right)
+        
+        recursion(root)
+        return minvalue
