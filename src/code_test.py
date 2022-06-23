8d749fcaf43c1b56844874e772192699ad96cb0a
kongyan66 <1042869685@qq.com>
2022-06-23 11:37:24 +0800

    [feat]:135.分发糖果

--- src/code_test.py
+++ src/code_test.py
@@ -1,11 +1,2 @@
-from multiprocessing import BoundedSemaphore
-
-
-n =3
-board = [['.'] * n for _ in range(n)]
-res = []
-for tem in board:
-    tem_str = "".join(tem)
-    res.append("".join(tem))
-print(res)
-
+for i in range(5, 1, -1):
+    print(i)
