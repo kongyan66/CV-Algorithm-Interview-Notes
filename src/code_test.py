99d4d54b623323257a6c65b86f253df0c4774ed2
kongyan66 <1042869685@qq.com>
2022-06-29 17:58:13 +0800

    [feat]:70.爬楼梯

--- src/code_test.py
+++ src/code_test.py
@@ -1,2 +1,11 @@
-tup = tuple("etttrr")
-print(tup)
+def climbStairs(n: int) -> int:
+    if n <= 2:
+        return n
+    p, q, r = 1, 2, 1
+    for i in range(3, n+1):
+        p, q = q, r
+        r = p + q
+        print(q)
+        print((p, q))
+    return r
+print(climbStairs(4))
