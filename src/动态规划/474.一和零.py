b00adf08c5c552147be7bed7a2f78c997500992d
kongyan66 <1042869685@qq.com>
2022-07-04 21:46:19 +0800

    [feat]:474.一和零

---
+++ src/动态规划/474.一和零.py
@@ -0,0 +1,25 @@
+# 题目：给一个字符串列表strs，及整数m,n, 请找出并返回strs的最大子集的大小，要求该子集最多有m个0以及n个1
+
+# 思路：自己看真看不出来是背包问题，这题特殊在有两个背包，m装1，n装0
+# 物品就是字符串了，要求子集最大，那么价值就是字符串的个数了(这里每个物品价值都为1了），重量就是0或者1的数量
+
+'''
+1. dp[i][j] 表示装i个0，j个1, 所得到的的最大价值(子集长度最大)
+2. dp[i][j] = max(dp[i][j], dp[i-zeroNum][j-oneNum] + 1)
+   这里和dp[j] = max(dp[j], dp[j-weight[i] + value[i]]) 完全一致
+3. dp[i][j] = 0
+4.遍历顺序，背包都是逆序
+'''
+
+# 解
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        dp = [[0] * (n+1) for _ in range(m+1)]
+        dp[0][0] = 0
+        for item in strs:
+            zeroNums = item.count('0')
+            oneNums = item.count('1')
+            for i in range(m, zeroNums-1, -1):
+                for j in range(n, oneNums-1, -1):
+                    dp[i][j] = max(dp[i][j], dp[i-zeroNums][j -oneNums] + 1)
+        return dp[m][n]
