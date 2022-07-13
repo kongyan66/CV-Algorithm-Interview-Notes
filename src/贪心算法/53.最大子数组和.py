021fb3e9a845be87dda9ecce18385fb9cc9b226d
kongyan66 <1042869685@qq.com>
2022-07-13 10:10:16 +0800

    [feat]:53.最大子数组和—DP

--- src/贪心算法/53.最大子数组和.py
+++ src/贪心算法/53.最大子数组和.py
@@ -33,9 +33,10 @@ dp[0] = 0
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         dp = [0] * (len(nums) + 1)
-        dp[0] = nums[0] 
+        dp[0] = nums[0]          # dp[i]依赖于dp[i-1]状态，从dp[0]开始
         res = dp[0]
         for i in range(1, len(nums)):
             dp[i] = max(dp[i-1] + nums[i], nums[i])
             res = max(res, dp[i])
         return res
+        
