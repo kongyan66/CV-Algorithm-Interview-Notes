44aff33116180c365e417c9eb97b998ef77f330d
kongyan66 <1042869685@qq.com>
2022-07-13 10:09:38 +0800

    53.最大子数组和—DP

--- src/贪心算法/53.最大子数组和.py
+++ src/贪心算法/53.最大子数组和.py
@@ -3,7 +3,7 @@
 # 思路：最简单就是所有子序列算一次sum，然后比较，但很费时间
 # 局部最优：当前“连续和”为负数的时候立刻放弃，从下一个元素重新计算“连续和”，因为负数加上下一个元素 “连续和”只会越来越小。
 
-# 解法
+# 贪心解法
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         # 使用count实时记录子序列和，当其小于0时就要归零重新开始计。
@@ -19,3 +19,23 @@ class Solution:
                 count = 0
                 
         return result
+
+# 动态规划
+'''
+1.确定dp数组及下标含义
+dp[i] 表示nums[:i+1] 最大连续子序列和为dp[i]
+2.确定推导公式
+dp[i]有两个方向，要么继续加，要么重头开始，取值最大的
+dp[i] = max(dp[i-1]]+nums[i-1], nums[i])
+3.初始化
+dp[0] = 0
+'''
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        dp = [0] * (len(nums) + 1)
+        dp[0] = nums[0] 
+        res = dp[0]
+        for i in range(1, len(nums)):
+            dp[i] = max(dp[i-1] + nums[i], nums[i])
+            res = max(res, dp[i])
+        return res
