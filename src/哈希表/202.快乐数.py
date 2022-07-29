5b2a6a2d5882df391e55bfb7cef41f6c91d26283
kongyan66 <1042869685@qq.com>
2022-07-29 21:08:37 +0800

    [re]:202.快乐数

--- src/哈希表/202.快乐数.py
+++ src/哈希表/202.快乐数.py
@@ -18,3 +18,26 @@ class Solution:
           return False
         else:
           recorde.add(n)  
+
+# re-2 
+class Solution:
+    def __init__(self):
+        self.list_num = set()
+    def isHappy(self, n: int) -> bool:
+        return self.recursion(n)
+
+    # 1.确定入参与返回值
+    # 返回值： bool
+    def recursion(self, n):
+        # 2.确定递归停止条件
+        n_str = str(n)
+        sum = 0
+        for i in range(len(n_str)):
+            sum += int(n_str[i])**2
+        if sum in self.list_num:
+            return False
+        if sum == 1:
+            return True
+        # 单层递归逻辑
+        self.list_num.add(sum)
+        return self.recursion(sum)
