ad779107ca7eacd31f480d87671f3619811d4d66
kongyan66 <1042869685@qq.com>
2022-08-04 10:30:02 +0800

    [feat]:19.删除链表的倒数第N个节点

---
+++ src/链表/19.删除链表的倒数第N个节点.py
@@ -0,0 +1,18 @@
+# 思路：定位倒数第N个位置，因为链表都是正序的，所以首先需要知道节点总数，才能算出N节点的正序位置
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        # 为啥需要哨兵呢？应为链表中每个节点都有可能被删除
+        dummpy =  ListNode()
+        dummpy.next = head
+        fast = slow = dummpy
+        # fast先走（n+1)步
+        for _ in range(n+1):
+            fast = fast.next
+        # 然后fast slow同步走，当fast为空时，slow到达要删除节点的前一个位置
+        while fast:
+            fast = fast.next
+            slow = slow.next
+        # 删除节点，重新连接
+        slow.next = slow.next.next
+        return dummpy.next
