b96cf85f5bf425cbd4af6c63769268d765f465c8
kongyan66 <1042869685@qq.com>
2022-08-05 10:41:36 +0800

    189.轮转数组

---
+++ src/数组/189.轮转数组.py
@@ -0,0 +1,36 @@
+# 题目：给你一个数组，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+
+# 思路：
+'''
+本题是右旋转，其实就是反转的顺序改动一下，优先反转整个字符串，步骤如下：
+反转整个字符串
+反转区间为前k的子串
+反转区间为k到末尾的子串
+'''
+
+# 解法一： 空间复杂度O()
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for _ in range(k):
+            tem = nums.pop()
+            nums.insert(0, tem)
+
+# 解法二：
+class Solution:
+    def reverse(self, nums, l, r):
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums) 
+        k = k % n
+        self.reverse(nums, 0, n - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, n - 1)
