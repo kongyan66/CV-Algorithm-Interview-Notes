a44a86491d83f31da661254874f8cdb2bbeea7a3
kongyan66 <1042869685@qq.com>
2022-08-07 15:11:13 +0800

    [re]:1.两数之和

--- src/哈希表/1.两数之和.py
+++ src/哈希表/1.两数之和.py
@@ -1,3 +1,6 @@
+# 题目：求两数之和 时间复杂度小于O(n^2)
+
+# 思路：用map去避免重复计算
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
       redcord = dict()              # 用字典来保存值和索引，保证可以回头查，若是用list或set是达不到这个需求的
