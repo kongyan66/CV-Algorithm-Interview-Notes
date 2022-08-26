2808ab070ffb7578f08b7565bff5961836752a40
kongyan66 <1042869685@qq.com>
2022-08-26 21:09:01 +0800

    [re]:437.路径总和II

--- src/二叉树/437.路径总和III.py
+++ src/二叉树/437.路径总和III.py
@@ -4,30 +4,29 @@
 # 思路：参考560.和为k的数组，采用前缀和找路径，这里把数组换成了树，所以需要统计每个节点的前缀和，采用前序遍历，
 # 同时需要维护不同路径的和，所以需要用到回溯。
 
-# 解法：前缀和+递归+回溯
+# 解法：前缀和+前序遍历+回溯
 # 因为涉及向左走、向右走两条不同的路径，且两者互不影响，故应采用回溯算法
 class Solution:
     def __init__(self):
-        self.count = 0
         self.path_sum = collections.defaultdict(int)
-        self.sum_ = 0
-        
+        self.cur_sum = 0
+        self.count = 0
+
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
         self.path_sum[0] = 1
         self.recursion(root, targetSum)
-        print(self.path_sum)
         return self.count
 
     def recursion(self, root, targetSum):
         if not root:
-            return
-        # 这块和560.和为k的子数组完全一致
-        self.sum_ += root.val
-        self.count += self.path_sum[self.sum_ - targetSum]
-        self.path_sum[self.sum_] += 1
-        
+            return 
+        # 顺序不可颠倒，和560一致
+        self.cur_sum += root.val
+        self.count += self.path_sum[self.cur_sum - targetSum]
+        self.path_sum[self.cur_sum] += 1
+
         self.recursion(root.left, targetSum)
         self.recursion(root.right, targetSum)
-        # 回溯算法需要在递归后回溯，避免影响不同路径的递归
-        self.path_sum[self.sum_] -= 1        # 回溯
-        self.sum_ -= root.val                # 回溯
+        
+        self.path_sum[self.cur_sum] -= 1  # 回溯要对称
+        self.cur_sum -= root.val
