aebb4a15e225bf639478e9ac905bd49a88f640d6
kongyan66 <1042869685@qq.com>
2022-08-30 15:21:59 +0800

    [feat]:438.找到字符串中所有字母的异位词

--- src/code_test.py
+++ src/code_test.py
@@ -1,16 +1,8 @@
+from collections import Counter
+list_01 = [1,9,9,5,0,8,0,9]  #GNZ48-陈珂生日
+cnt = Counter(list_01)#Counter({9: 3, 0: 2, 1: 1, 5: 1, 8: 1})
+print(cnt[9])
 
-'''
-有一个串S，问是否能将其划分成m个不相交的连续子串，使得这些连续子串可以与要求的连续子串一 一对应。两个串相对应是指这两个串完全相等。例如"aab"="aab" 但 "aab"≠"baa"
-
-'''
-m, n = list(map(int, input().split()))
-s1 = input()
-arr = list(map(int, input().split()))
-s2 = []
-for _ in range(len(arr)):
-    s2.append(input())
-
-print(s2)
     
 
 
---
+++ src/字符串/438.找到字符串中所有字母异位词.py
@@ -0,0 +1,37 @@
+# 题目：给定两个字符串s和p，找到s中所有p的异位词的子串，返回这些子串的起始索引
+# 异位词 指由相同字母重排列形成的字符串（包括相同的字符串）。
+
+# 思路：滑动窗口 维护一个固定长len(p)的窗口，每次都去与p比较，若包含p的排列就保存窗口左端
+
+# 解
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        n, m = len(s), len(p)
+        if m > n:
+            return []
+        cnt = collections.Counter(p) # 哈希表：记录需要匹配到的各个字符的数目 返回是一个字典类型
+        need = m                     # 记录需要匹配到的字符总数【need=0表示匹配到了】
+        res = []
+
+        for right in range(n):
+            # 窗口右边界
+            ch = s[right]
+            if ch in cnt:
+                if cnt[ch] > 0:  # 新加入的字符位于p中
+                    nedd -= 1    # 此时新加入窗口中的字符对need有影响
+                cnt[ch] -= 1
+
+            # 窗口左边界
+            left = right - m
+            if left >= 0:
+                ch = s[left]
+                if ch in cnt:
+                    if cnt[ch] >= 0:   # 刚滑出的字符位于p中
+                        need += 1      # 此时滑出窗口的字符对need有影响
+                    cnt[ch] += 1
+ 
+            if need == 0:              # 找到了一个满足题意的窗口，其左端点为right-m+1
+                res.append(right - m + 1)
+        return res
+                    
+        
