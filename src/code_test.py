ea5ba386e26ed6cf078891004bc26da85c6a8f23
kongyan66 <1042869685@qq.com>
2022-09-03 09:36:57 +0800

    [re]:90.子集II

--- src/code_test.py
+++ src/code_test.py
@@ -1,2 +1,22 @@
-a = ['a', 'b', 'c']
-print(''.join(reversed(a)))
+class Solution:
+    def __init__(self):
+        self.path = []
+        self.result = []
+        self.used = []
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        self.backtracking(nums, 0)
+        return self.result
+   
+    def backtracking(self, nums, startindex):
+        self.result.append(self.path.copy())
+
+        if startindex >= len(nums):
+            return
+
+        for i in range(startindex, len(nums)):
+            self.path.append(nums[i])
+            self.backtracking(nums, i+1)
+            self.path.pop()
+        
+        
