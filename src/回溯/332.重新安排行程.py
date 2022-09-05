ba598339b41784f300403a4faec951f8d21a8ecb
kongyan66 <1042869685@qq.com>
2022-09-05 11:01:40 +0800

    [re]:332.重新安排行程

--- src/回溯/332.重新安排行程.py
+++ src/回溯/332.重新安排行程.py
@@ -8,8 +8,9 @@
 # 疑问二： 多条path怎么办呢
 
 class Solution:
+   
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        # 构建行程表，读出起点和终点，{key:start_point valuse:[end_point,]}
+        # 构建行程表，记录起点和终点，{key:start_point valuse:[end_point,]}
         tickets_dict = defaultdict(list)
         for item in tickets:
             tickets_dict[item[0]].append(item[1])
@@ -21,15 +22,13 @@ class Solution:
             # 也就是本题的result就是记录路径的（就一条），在如下单层搜索的逻辑中result就添加元素了。
             if len(path) == len(tickets) + 1:
                 return True
-            # 字母升序排列
-            tickets_dict[start_point].sort()
-            # 一个起始点，也可能有多个目的地
+
+            tickets_dict[start_point].sort()   # 字母升序排列
+            # DFS找所有路径
             for _ in tickets_dict[start_point]:
-                # 用掉了就删除，避免死循环
-                end_point = tickets_dict[start_point].pop(0) 
-                # 找到一个相邻地，加入path
-                path.append(end_point)
-                # 
+                end_point = tickets_dict[start_point].pop(0)  # 目的地用一次就删除，避免死循环
+                path.append(end_point)               # 找到一个相邻地，加入path
+                #  
                 if backtracking(end_point):
                     return True
                 path.pop()  # 回溯  回退上一状态
@@ -37,3 +36,34 @@ class Solution:
 
         backtracking("JFK")
         return path
+
+# re-3
+from collections import defaultdict
+class Solution:
+    def __init__(self):
+        self.path = ["JFK"]
+        self.tickets = []
+
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        # 构建行程表，读出起点和终点，{key:start_point valuse:[end_point,]}
+        tickets_dict = defaultdict(list)
+        self.tickets = tickets
+        for ticket in tickets:
+            tickets_dict[ticket[0]].append(ticket[1])
+        
+        self.backtracking(tickets_dict, 'JFK')
+
+        return self.path
+    
+    def backtracking(self, tickets_dict, start_point):
+        if len(self.path) == len(self.tickets) + 1:
+            return True
+        tickets_dict[start_point].sort()
+        for _ in range(len(tickets_dict[start_point])):
+            end_point = tickets_dict[start_point].pop(0)
+            self.path.append(end_point)
+            # 本题我们只需要一条符合条件的路径，找到即返回且结束所有递归
+            if self.backtracking(tickets_dict, end_point):  
+                return True
+            self.path.pop()
+            tickets_dict[start_point].append(end_point)
