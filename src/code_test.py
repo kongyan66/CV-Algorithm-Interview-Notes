f812a12bfa1187893629f7b782378b767905a794
kongyan66 <1042869685@qq.com>
2022-09-29 22:23:44 +0800

    [doc]:笔试更新-集度

--- src/code_test.py
+++ src/code_test.py
@@ -1,9 +1,13 @@
-a = [1, [2, 2], 3]
-b = a.copy()
+from collections import defaultdict
+def finOddNum(arr):
+    dictNum = defaultdict(int)
+    for num in arr:
+        dictNum[num] += 1
+    for key, val in dictNum.items():
+        if val % 2 == 1:
+            return key
 
-b[1][1] = 7
-
-print(a)
-print(b)
-
-    
+if __name__ == '__main__':
+    arr = [1, 2, 1, 5, 7, 3, 4, 2, 3, 4, 3, 5, 7, 8, 8]
+    ans = finOddNum(arr)
+    print(ans)
