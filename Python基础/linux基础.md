6e495b32a016c9fdcd900a2f50124808ac0e581a
kongyan66 <1042869685@qq.com>
2022-09-30 21:49:43 +0800

    [doc]:笔试更新-京东

---
+++ Python基础/linux基础.md
@@ -0,0 +1,5 @@
+## 修改目录权限
+
+[Linux修改目录权限](https://www.jianshu.com/p/ac4e994a47e2)
+
+<img src="https://raw.githubusercontent.com/kongyan66/Img-for-md/master/img/image-20220929222723402.png" alt="image-20220929222723402" style="zoom:80%;" />
--- src/code_test.py
+++ src/code_test.py
@@ -1,13 +1,52 @@
-from collections import defaultdict
-def finOddNum(arr):
-    dictNum = defaultdict(int)
-    for num in arr:
-        dictNum[num] += 1
-    for key, val in dictNum.items():
-        if val % 2 == 1:
-            return key
+n = int(input())
+arr = list(map(int, input().split()))
+sum = 0
+def findmid(arr):
+    n = len(arr)
+    if n == 1:
+        return arr[0]
+    elif n % 2 == 0:
+        return min(arr[n // 2 - 1], arr[n // 2])
+    else:
+        return arr[n // 2]
+for i in range(n):
+    for j in range(i, n):
+        tem = arr[i:j+1]
+        tem.sort()
+        mid = findmid(tem)
+        sum += mid
+print(sum)
+
+n = 2
+map = ['a']
+for i in range(25):
+    map.append(chr(ord(map[-1]) + 1))
+
+path = []
+sum = 0
+
+def dfs(arr):
+    global sum
+    if len(path) == n:
+        weight = findweight(path)
+        sum += weight
+        return 
+    
+    for i in range(len(map)):
+        path.append(map[i])
+        dfs(arr)
+        path.pop()
+
+def findweight(path):
+    weight = 0
+    length = len(path)
+    for i in range(0, length - 1):
+        if path[i] != path[i+1]:
+            weight += 1
+    return weight
+            
 
 if __name__ == '__main__':
-    arr = [1, 2, 1, 5, 7, 3, 4, 2, 3, 4, 3, 5, 7, 8, 8]
-    ans = finOddNum(arr)
-    print(ans)
+    dfs(map)
+    print(sum)
+
