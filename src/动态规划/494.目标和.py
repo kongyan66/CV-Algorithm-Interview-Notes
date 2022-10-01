a5a17f88de279755a0e912418bc85430d1ac7faf
kongyan66 <1042869685@qq.com>
2022-10-01 21:22:50 +0800

    [re]:494.目标和

--- src/动态规划/494.目标和.py
+++ src/动态规划/494.目标和.py
@@ -22,7 +22,8 @@ i=4，还有dp[0]种装法
 5.打印dp检查
 '''
 
-# 解法一： 动态规范
+# 解法一： 动态规范 
+# 01背包(决定遍历顺序) 组合问题（决定状态转移式）
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         total = sum(nums)
@@ -36,3 +37,54 @@ class Solution:
                 dp[j] += dp[j - nums[i]]
         return dp[bagsize]
 
+# 解法二：回溯（超时，无法AC）
+# 枚举出所有情况，即加+和-的所有情况，然后看符合和为target的路径的数量
+class Solution:
+    def __init__(self):
+        self.path = []
+        self.map = [-1, 1]
+        self.count = 0
+        self.target = 0
+
+    def findTargetSumWays(self, nums: List[int], target: int) -> int：
+        self.target = target
+        self.dfs(nums, 0)
+        return self.count
+
+    def dfs(self, nums, index):
+        if len(self.path) == len(nums):
+            if sum(self.path) == self.target:
+                self.count += 1
+            return
+        
+        for i in range(len(self.map)):
+            self.path.append(nums[index] * self.map[i])
+            self.dfs(nums, index + 1)
+            self.path.pop()
+
+# 解法三：回溯+记忆化优化（不太好懂啊）
+# 参考：https://leetcode.cn/problems/target-sum/solution/by-flix-rkb5/
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        cache = {} # 记忆化单元
+        # @functools.cache  # python functions 自带记忆化单元，启用后可以省去自定义cache单元
+        def dfs(i, summ, target):
+            if (i, summ) in cache:       # 记忆化：已存在，直接返回
+                return cache[(i, summ)] 
+            
+            if i == len(nums):           # 遍历完了全部的元素，递归中止
+                if summ == target:       # 找到了一个满足要求的组合
+                    cache[(i, summ)] = 1
+                else:
+                    cache[(i, summ)] = 0
+                return cache[(i, summ)]
+
+            pos = dfs(i + 1, summ + nums[i], target)   # nums[i]前面添加'+'号
+            neg = dfs(i + 1, summ - nums[i], target)
+            cache[(i, summ)] = neg + pos               # 以上两种情况的组合数之和
+
+        return dfs(0, 0, target)
+
+
+
+ 
