baabe601f39e3223b6448542c7b58e24100e5367
kongyan66 <1042869685@qq.com>
2022-10-02 16:41:43 +0800

    [doc]:03_深拷贝与浅拷贝

--- src/code_test.py
+++ src/code_test.py
@@ -1,17 +1,11 @@
-from functools import wraps
-# 装饰器
-def logged(func):
-    @wraps(func)
-    def with_logging(*args, **kwargs):
-        return func(*args, **kwargs)
-    return with_logging
+from xml.etree.ElementTree import ProcessingInstruction
 
-# 函数
-@logged
-def f(x):
-   """does some math"""
-   return x + x * x
 
-f(2)
-print(f.__name__)      # 输出 'f'
-print (f.__doc__)       # 输出 'does some math'
+def a():
+    a = 289
+    b = 289
+    print(a is b)
+    print(id(a))
+    print(id(b))
+
+a()
