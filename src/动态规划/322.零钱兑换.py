b9256c53750b57f5f74b1d6f3384216666c999c3
kongyan66 <1042869685@qq.com>
2022-10-02 10:41:49 +0800

    [re]:322.零钱兑换

--- src/动态规划/322.零钱兑换.py
+++ src/动态规划/322.零钱兑换.py
@@ -11,10 +11,11 @@
 # 解法  完全背包 最小值问题
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # 我们要取最小值，所以初始化必须放最大的，观察得知最小数量小于等于amount(比如硬币面额都为1时为相等条件)
+        # 我们要取最小值，所以初始化必须放最大的
         dp = [amount+1] * (amount + 1)
         dp[0] = 0
         for i in range(len(coins)):
             for j in range(coins[i], amount+1):
                 dp[j] = min(dp[j], dp[j-coins[i]] + 1)   # 注意这里加1不是coins[i],这取决于dp的含义
+        # 观察得知最小数量小于等于amount(比如硬币面额都为1时为相等条件)，如果大于了，说明就没找到
         return dp[amount] if dp[amount] < amount+1 else -1  # 如果找不到，那么初始值就不会替换掉，也是个小细节呀
