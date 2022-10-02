1e818fdaa5d46cbc7f79d46489b2197762dfdc05
kongyan66 <1042869685@qq.com>
2022-10-02 19:26:02 +0800

    [re]:python可变对象与不可变对象

--- src/code_test.py
+++ src/code_test.py
@@ -1,11 +1,6 @@
-from xml.etree.ElementTree import ProcessingInstruction
-
-
-def a():
-    a = 289
-    b = 289
-    print(a is b)
-    print(id(a))
-    print(id(b))
-
-a()
+print(id(258))  
+a = 258  
+print(id(a))  
+b = 258  
+print(id(b))  
+print(a is b)  
