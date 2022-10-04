c48fbc551e0efeacc606766b6269d603c288c522
kongyan66 <1042869685@qq.com>
2022-10-04 19:34:47 +0800

    [doc]:最优问题之动态规划

--- src/code_test.py
+++ src/code_test.py
@@ -1,4 +1,23 @@
-s = '1'
-s += '2'
 
-print(s)
+
+def coinChange(coins, amount):
+    return dfs(coins, amount)
+
+def dfs(coins, amount):
+    if amount == 0:
+        return 0
+    if amount < 0:
+        return -1
+    res = float('inf')
+    for coin in coins:
+        subProblem = dfs(coins, amount - coin)
+        if subProblem == -1:
+            continue
+        res = min(res, subProblem + 1)
+    return res
+
+if __name__ == '__main__':
+    coins = [1, 2, 5]
+    amount = 11
+    ans = coinChange(coins, amount)
+    print(ans)
