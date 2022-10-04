ce8821c5faa8132a5d622645baeb0c42142e09fc
kongyan66 <1042869685@qq.com>
2022-10-04 10:24:41 +0800

    [re]:300.最长递增子序列

--- src/动态规划/300.最长递增子序列.py
+++ src/动态规划/300.最长递增子序列.py
@@ -11,15 +11,15 @@ dp[i] = max(dp[i], dp[j]+1)
 # 解法
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        if len(nums) <= 1:
-            return len(nums)
         dp = [1] * (len(nums))  # nums长度大于1
         result = 0
         
         for i in range(1,len(nums)):
             for j in range(i):
-                if nums[i] > nums[j]:  # 只有
-                    dp[i] = max(dp[i], dp[j]+1)
+                # 寻找 nums[0..j-1] 中比 nums[i] 小的元素
+                if nums[i] > nums[j]:  
+                    # 把 nums[i] 接在后面，即可形成长度为 dp[j] + 1，且以 nums[i] 为结尾的递增子序列
+                    dp[i] = max(dp[i], dp[j]+1)  # max()中dp[i]笔试i=0-j时的值，实时在变，我们要选取最大的。
             result = max(dp[i], result)   # 并不是最后一个是最长的，所以要一直取最大值
         return result
        
